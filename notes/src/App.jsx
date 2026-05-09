192e50fb48b6a3c63464a731d2d92e763393b232
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-05-09 14:05:48 +0800

    note services

--- notes/src/App.jsx
+++ notes/src/App.jsx
@@ -1,75 +1,72 @@
 import Note from './component/Notes'
-import { useState, useEffect} from 'react'
+import {useState, useEffect} from 'react'
 import axios from 'axios'
+import noteService from './noteService'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNotes, setNewNotes] = useState('')
   const [showAll, setShowAll] = useState(true)
 
-  const hook = () => {
-    axios
-      .get('http://localhost:3001/notes')
+  useEffect(()=>{
+    noteService
+      .getAll()
       .then(response=>
         setNotes(response.data)
-      )
-  }
+      )}, []
+  )
 
-  useEffect(hook,[])
-
-  const handleSubmit = (event) => {
+  const handleSubmit = event => {
     event.preventDefault()
     const newObject = {
-      content: newNotes,
-      important: Math.random() > 0.5,
+      content : newNotes,
+      important : Math.random() > 0.5,
     }
-    axios
-      .post('http://localhost:3001/notes', newObject)
-      .then(response=>{
-          setNotes(notes.concat(response.data))
-          setNewNotes('')
-        }
+    noteService
+      .create(newObject)
+      .then(response=>
+        setNotes(notes.concat(response.data))
       )
+    setNewNotes('')
   }
 
-  const handleInputChange = (event) => {
+  const handleNoteChange = event => {
     const inputVal = event.target.value
     setNewNotes(inputVal)
   }
 
-  const filteredNotes = showAll ? 
-    notes 
-      : 
-    notes.filter(note=>note.important)
+  const filterNotes = showAll ?
+    notes :
+    notes.filter(n=>n.important)
 
-    const toggleImportant = id => {
-      const url = `http://localhost:3001/notes/${id}`
-      const note = notes.find(n=>n.id===id)
-      const changedNote = {...note, important: !note.important}
-
-      axios
-        .put(url,changedNote)
-        .then(response=>
-          setNotes(notes.map(note=>note.id===id ? response.data : note))
-        )
-    }
+  const toggleImportant = id => {
+    const note = notes.find(n=>n.id===id)
+    const changedNote = {...note, important : !note.important}
+    noteService
+      .update(id, changedNote)
+      .then(response=>
+        setNotes(notes.map(n=>n.id===id?response.data:n))
+      )
+  }
 
   return(
     <div>
       <h1>Notes</h1>
-      <button onClick={()=>setShowAll(!showAll)}> 
-        show {showAll ? 'important': 'all'}
-      </button>
+      <button onClick={()=>setShowAll(!showAll)}>show {showAll ? 'important': 'all'}</button>
       <ul>
-        {filteredNotes.map(note=>
-          <Note key={note.id} 
-          toggleImportance={()=>toggleImportant(note.id)} 
-          note={note}/>
-        )}
+        {
+        filterNotes.map(note=>
+        <Note 
+          key={note.id} 
+          toggleImportant={()=>toggleImportant(note.id)}
+          note={note}
+        />)
+        }
       </ul>
+
       <div>
         <form onSubmit={handleSubmit}>
-          <input value={newNotes} onChange={handleInputChange} />
+          <input value={newNotes} onChange={handleNoteChange}/>
           <button type="submit">Save</button>
         </form>
       </div>
