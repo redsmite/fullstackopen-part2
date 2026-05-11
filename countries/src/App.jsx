1b519f017a1fc28cad7d3c3a3450f6db7c9ef12a
Kym Carabeo <yuudachipoikancolle@gmail.com>
2026-05-11 20:41:58 +0800

    exercise recreate exercise 2.18 no AI

--- countries/src/App.jsx
+++ countries/src/App.jsx
@@ -1,9 +1,9 @@
-import data from './data'
 import {useState, useEffect} from 'react'
+import data from './data'
 
 const App = () =>{
-  const [ countryAll, setCountryAll ] = useState([])
-  const [ search, setSearch ] = useState('')
+  const [countryAll, setCountryAll] = useState([])
+  const [filter, setFilter] = useState('')
 
   useEffect(()=>{
     data
@@ -13,46 +13,40 @@ const App = () =>{
       )
   },[])
 
-  const handleFilterChange = e => setSearch(e.target.value)
+  const renderedOutput = () => {
+    const filteredCountries = countryAll.filter(c=>
+      c.name.common.toLowerCase().includes(filter.toLowerCase())
+    )
 
-  const filteredCountry = countryAll.filter(country=>
-    country.name.common.toLowerCase().includes(search.toLowerCase())
-  )
-
-  const renderResult = () => {
-    if (!search) {
+    if(!filter){
       return null
     }
 
-    if (filteredCountry.length >= 10) {
+    if(filteredCountries.length > 10){
       return <p>Too many matches, specify another filter</p>
-    } 
-    
-    if (filteredCountry.length > 1 && filteredCountry.length < 10) {
-      return (
-        filteredCountry.map(c=>
-          <p key={c.ccn3}>{c.name.common}</p>
-        )
-      ) 
     }
 
-    if (filteredCountry.length === 1){
-      const c = filteredCountry[0]
+    if(filteredCountries.length > 1) {
+      return filteredCountries.map(c=>
+        <p key={c.ccn3}>{c.name.common}</p>
+      )
+    }
+
+    if(filteredCountries.length===1){
+      const c = filteredCountries[0]
       return (
         <>
           <h1>{c.name.common}</h1>
-          <p>Capital: {c.capital}</p>
-          <p>Area: {c.area} km^2</p>
-          <p>Languages</p>
+          <p>Capital {c.capital}</p>
+          <p>Area {c.area}</p>
+          <h3>Languages</h3>
           <ul>
-            {Object.values(c.languages).map(l=>
-              <li key={l}>{l}</li>
+            {Object.values(c.languages).map(c=>
+              <li key={c}>{c}</li>
             )}
           </ul>
-          <img 
-          src={c.flags.png}
-          style={{width:"150px", border:"1px solid lightgrey"}}
-           />
+          <img src={c.flags.png}
+          style={{width:"150px"}} />
         </>
       )
     }
@@ -60,13 +54,12 @@ const App = () =>{
 
   return(
     <div>
-      <p>find countries <input onChange={handleFilterChange} value={search} /></p>
+      <p>find countries <input value={filter} onChange={e=>setFilter(e.target.value)} /></p>
       <div>
-        {renderResult()}
+        {renderedOutput()}
       </div>
     </div>
   )
-
 }
 
 export default App
--- countries/src/data.js
+++ countries/src/data.js
@@ -3,13 +3,17 @@ import axios from 'axios'
 const baseUrl = 'https://studies.cs.helsinki.fi/restcountries/api/'
 
 const getAll = () => {
-    const request = axios.get(`${baseUrl}/all`)
-    return request.then(response=>response.data)
+    const result = axios.get(`${baseUrl}/all`)
+    return result.then(response=>
+        response.data
+    )
 }
 
-const get = name => {
-    const request = axios.get(`${baseUrl}/name/${name}`)
-    return request.then(response=>response.data)
+const get = filter => {
+    const result = axios.get(`${baseUrl}/name/${filter}`)
+    return result.then(response=>
+        response.data
+    )
 }
 
-export default { getAll, get }
+export default {getAll,get}
